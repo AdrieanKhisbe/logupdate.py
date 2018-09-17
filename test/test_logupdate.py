b6f08583c857a02a07b40640b95584ed084de738
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-17 17:45:53 +0200

    Use pyte to evaluate ascii characters

--- test/test_logupdate.py
+++ test/test_logupdate.py
@@ -1,15 +1,27 @@
 from logupdate import logupdate
 import io
-from subprocess import run, PIPE
+from pyte import Screen, Stream
+
+def project_output(stream):
+    screen = Screen(80, 24)
+    terminal_stream = Stream(screen)
+    terminal_stream.feed(stream.getvalue())
+    return screen.display
 
 def test_append_simple_message():
     stream = io.StringIO()
     logupdate_function = logupdate.create(stream)
     logupdate_function("Hello")
+    output = project_output(stream)
+    assert output[0].strip() == "Hello"
 
-    output = run(["echo", "-n", stream.getvalue()], shell=stdout=PIPE)
-    print(output)
-    print(output.stdout)
-    print(output.stderr)
-    print(dir(output))
-    assert output == "Hello\n"
+def test_append_several_message():
+    stream = io.StringIO()
+    logupdate_function = logupdate.create(stream)
+    logupdate_function("Hello")
+    logupdate_function("Hola")
+    logupdate_function("Bonchour")
+    logupdate_function("Bonjour")
+
+    output = project_output(stream)
+    assert output[0].strip() == "Bonjour"
