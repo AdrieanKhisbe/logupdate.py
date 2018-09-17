1ef2e30554795db8bd007bf432a5af96759eedb6
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-17 07:54:39 +0200

    Start to draft the logupdate function

--- logupdate/__init__.py
+++ logupdate/__init__.py
@@ -1,2 +1,20 @@
+import sys
+import cursor
 
-print('logupdate')
+class LogUpdate():
+
+    def __init__(self, stream, options = None):
+        self.options = {"show_cursor": False, **(options or {})}
+        self.stream = stream
+        self.prev_line_count = 0
+
+    def render(self, *message):
+        if not self.options["show_cursor"]:
+            cursor.hide()
+        output = " ".join(message) + "\n"
+        print(output)
+
+    def __call__(self, *message):
+        self.render(*message)
+
+logupdate = LogUpdate(sys.stdout)
---
+++ simple_example.py
@@ -0,0 +1,6 @@
+from logupdate import logupdate
+from time import sleep
+
+logupdate("Hello")
+sleep(2)
+logupdate("Bye")
