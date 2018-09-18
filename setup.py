35474eeeb8df4d9313672d38c8e2ae69d989353b
Adriean Khisbe <adriean.khisbe@live.fr>
2018-09-18 09:21:36 +0200

    Release a 0.2 version with chained calls

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="logupdate",
-    version="0.1.1",
+    version="0.2.0",
     author="Adriean Khisbe",
     author_email="adriean.khisbe@live.fr",
     description="Log by overwriting the previous output in the terminal",
